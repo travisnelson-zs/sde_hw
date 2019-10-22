e606ea47d0f4ea51c68f5021c170ada22fc95137
Travis Nelson <travisjamesnelson@gmail.com>
2019-10-22 07:49:44 -0700

    creating tests...

--- wishlist_app/wsgi.py
+++ wishlist_app/wsgi.py
@@ -1,10 +1,11 @@
 from application import create_app
 from os import path
 
+# Create a sample db populated with initial data to play with
 if not path.isfile('book_wishlists.db'):
     import sys
     sys.path.append("..")
-    import init_db
+    import init_demo_db
 
 app = create_app()
 
